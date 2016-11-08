35b7dd9da12a8d06ab20153de89d5a1ccb28fc3e
ligoriosalgado <lsalgadof93@gmail.com>
2016-11-07 19:46:33 -0600

    classed base views

--- Site/urls.py
+++ Site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views import *
+from .classed_views import Index,Register
 urlpatterns = [
 	url(r'^$', index, name="sites-index"),
     url(r'^contacto/$',contacto, name="sites-contacto"),
@@ -7,6 +8,10 @@ urlpatterns = [
     url(r'^login/$',login, name="sites-login"),
     url(r'^profile/$',profile, name="sites-profile"),
     url(r'^logout/$',logout, name="sites-logout"),
+    url(r'^index2/$',Index.as_view(), name="index2"),
+    url(r'^register2/$',Register.as_view(), name="register2"),
+
+
 
     
 ]
